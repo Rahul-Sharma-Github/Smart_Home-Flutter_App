492074fd783d9173b43ac605b0d3ee67eea0b6d6
Rahul <rahulsharma3258@gmail.com>
2023-04-09 18:39:18 +0530

    initial version 1.0

---
+++ lib/widget_ui/bottom_navigation_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 20.0),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 30.0,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.grey[600],
+        selectedItemColor: Colors.grey[800],
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_outlined),
+            label: 'Person',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wb_sunny_outlined),
+            label: 'Temprature',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_none),
+            label: 'Notification',
+          ),
+        ],
+      ),
+    );
+  }
+}
